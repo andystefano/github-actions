dd2a978aa6071945fafa56e7494f067a8fa4615d
Andy Hormazabal <andy@andy.cl>
2025-05-30 13:34:24 -0400

    actualizacion script diseno 4

--- 1.php
+++ 1.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-echo "La fecha y hora actual es ahhhh??::: ###***// bien......?????";
+echo "La fecha y hora actual es ahhhh??::: ###***// bien......seguro que si";
 echo date('Y-m-d H:i:s');
 
 ?>
