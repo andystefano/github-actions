9d2cce4ddaeacf2be3259aadd3b5b370c7eaa06f
ANDY Hormazabal <andy@andy.cl>
2025-05-31 01:44:09 -0400

    prueba ok

--- 1.php
+++ 1.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-echo "La fecha y hora actual es ahhhh??::: ###***// bien......seguro que si sisisisisisdddd";
+echo "La fecha y hora actual es ahhhh??::: ###***// bien......seguro que SIII";
 echo date('Y-m-d H:i:s');
 
 ?>
