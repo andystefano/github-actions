c322aecf6913873e91692d80f066d81e0306b38f
ANDY Hormazabal <andy@andy.cl>
2025-05-31 01:49:00 -0400

    fix script

--- 1.php
+++ 1.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-echo "La fecha y hora actual es ahhhh??::: ###***// bien......seguro que SIII";
+echo "La fecha y hora actual es ahhhh??::: ###***// bien......seguro que SIIIIII";
 echo date('Y-m-d H:i:s');
 
 ?>
