8d59c2ea895a5671d430f5868685274eae109471
ANDY Hormazabal <andy@andy.cl>
2025-08-22 01:01:56 -0400

    update deploid fix slint

--- 1.php
+++ 1.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-echo "La fecha y hora actual es ahhhh??::: ###***// bien......seguro que SIpueIIIII";
+echo "La fecha y hora actual es ahhhh?????::: ###***// bien......seguro que SIpueIIIII";
 echo date('Y-m-d H:i:s');
 
 ?>
